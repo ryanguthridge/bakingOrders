c64218c1c26345411ce1caa03b41576c5497dd91
Ryan Guthridge <ryanguthridge@gmail.com>
2014-10-07 22:26:04 -0400

    Adding D3 source stuff and invoice view starters

---
+++ deleteOrder.php
@@ -0,0 +1,25 @@
+<?php
+	$uuid = $_POST["uuid"];
+	
+	include_once("config.php");
+	
+	/* check connection */
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}else{
+		//printf("Connected");
+	}
+	
+	$query = "DELETE FROM orders WHERE uuid = '$uuid'";
+	
+	if ($result = $mysqli->query($query)) {
+		
+		echo("$(\"#submitMessage\").html(\"<div class='alert alert-danger' role='alert'><p align='center'><b>Good job, dingus</b>! The order was successfully deleted, you will be redirected to <a href='orderGrid.php'>Order History</a> in 4 seconds.</p></div>\");");
+		echo("window.location.href = \"#submitMessage\";");
+		echo("setTimeout(function(){window.location.href=\"orderGrid.php\";}, 4000);");
+		
+	}else{
+		echo("$(\"#submitMessage\").html(\"<div class='alert alert-warning' role='alert'><p align='center'><b>Well shit</b>! I don't think that the order was successfully deleted.</p></div>\");");
+	}
+?>
---
+++ invoiceView.php
@@ -0,0 +1,48 @@
+<?php
+include_once("config.php");
+		
+			/* check connection */
+			if ($mysqli->connect_errno) {
+				printf("Connect failed: %s\n", $mysqli->connect_error);
+				exit();
+			}else{
+				//printf("Connected");
+			}
+			
+			$query = 'SELECT uuid,name,phone,email,address,cost,contactDate,dueDate,orderItems,notes,deliveryFee,rentalFee,rentalItems,paid,totalCost FROM orders WHERE uuid = "'.$search.'"';
+				
+			if ($result = $mysqli->query($query)) {
+
+				/* fetch associative array */
+				while ($row = $result->fetch_assoc()) {
+					printf("<script>$(\"#name\").val(\"%s\")</script>",$row["name"]);
+					printf("<script>$(\"#phone\").val(\"%s\")</script>",$row["phone"]);
+					printf("<script>$(\"#email\").val(\"%s\")</script>",$row["email"]);
+					printf("<script>$(\"#address\").val(\"%s\")</script>",$row["address"]);
+					printf("<script>$(\"#cost\").val(\"%s\")</script>",$row["cost"]);
+					printf("<script>$(\"#uuid\").val(\"".$search."\")</script>");
+					printf("<script>$(\"#contactDate\").val(\"%s\")</script>",$row["contactDate"]);
+					printf("<script>$(\"#dueDate\").val(\"%s\")</script>",$row["dueDate"]);
+					printf("<script>$(\"#orderItems\").val(\"%s\")</script>",$row["orderItems"]);
+					printf("<script>$(\"#deliveryFee\").val(\"%s\")</script>",$row["deliveryFee"]);
+					printf("<script>$(\"#rentalFee\").val(\"%s\")</script>",$row["rentalFee"]);
+					printf("<script>$(\"#rentalItems\").val(\"%s\")</script>",$row["rentalItems"]);					
+					printf("<script>$(\"#totalCost\").val(\"%s\")</script>",$row["totalCost"]);	
+					printf("<script>$(\"#notes\").val(\"%s\")</script>",$row["notes"]);
+					printf("<script>$(\"#paid\").val(\"%s\")</script>",$row["paid"]);
+				}
+				
+				echo("<script>");
+				echo("$(\"#submitMessage\").html(\"<div class='alert alert-success' role='alert'><p align='center'><b>Congratulations!</b> The order was successfully loaded.</p>\");");
+				echo("orderUpdate = true;");
+				echo("$(\"#submitButton\").text(\"Update Order\");");
+				echo("</script>");
+
+				/* free result set */
+				$result->free();
+			}else{
+				printf ("Database connect error");
+			}
+			//close db connection	
+			$mysqli->close();
+?>
